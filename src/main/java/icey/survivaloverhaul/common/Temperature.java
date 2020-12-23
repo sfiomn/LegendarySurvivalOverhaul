dfc2f01879d7b517117eec332797f4905c17183a
Icey <Penguixia@gmail.com>
2020-12-23 09:31:29 -0500

    Actually renamed root folder to SurvivalOverhaul, updated some relevant files.

---
+++ src/main/java/icey/survivaloverhaul/common/Temperature.java
@@ -0,0 +1,19 @@
+package icey.survivaloverhaul.common;
+
+import java.util.function.Predicate;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.MutableBoundingBox;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+
+@SuppressWarnings("unused")
+public class Temperature
+{
+	
+}
