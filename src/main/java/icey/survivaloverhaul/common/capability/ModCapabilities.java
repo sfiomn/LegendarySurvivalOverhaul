422394d8afab0c36383945081c11b64b3f0a89f1
Icey <Penguixia@gmail.com>
2021-01-14 12:53:33 -0500

    Merged attach capabilities class and capability update class into one class
    Merged AttachCapabilities and CapabilityUpdateAndSync into one class

--- src/main/java/icey/survivaloverhaul/common/capability/ModCapabilities.java
+++ src/main/java/icey/survivaloverhaul/common/capability/ModCapabilities.java
@@ -2,24 +2,39 @@ package icey.survivaloverhaul.common.capability;
 
 import icey.survivaloverhaul.Main;
 import icey.survivaloverhaul.common.capability.temperature.TemperatureCapability;
+import icey.survivaloverhaul.common.capability.temperature.TemperatureProvider;
 import icey.survivaloverhaul.config.Config;
-import icey.survivaloverhaul.network.packets.UpdateTemperaturesPacket;
 import icey.survivaloverhaul.network.NetworkHandler;
-
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.world.World;
+import icey.survivaloverhaul.network.packets.UpdateTemperaturesPacket;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.TickEvent.Phase;
 import net.minecraftforge.event.TickEvent.PlayerTickEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent.PlayerChangedDimensionEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedInEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.network.PacketDistributor;
 
-@Mod.EventBusSubscriber(modid = Main.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
-public class CapabilityUpdateAndSync
+@Mod.EventBusSubscriber(modid = Main.MOD_ID, bus = EventBusSubscriber.Bus.FORGE)
+public class ModCapabilities
 {
+	public static final ResourceLocation TEMPERATURE_RES = new ResourceLocation(Main.MOD_ID, "temperature");
+	
+	@SubscribeEvent
+	public static void attachCapability(AttachCapabilitiesEvent<Entity> event)
+	{
+		if (event.getObject() instanceof PlayerEntity)
+		{
+			event.addCapability(TEMPERATURE_RES, new TemperatureProvider());
+		}
+	}
+
 	@SubscribeEvent
 	public static void onPlayerTick(PlayerTickEvent event)
 	{
@@ -38,7 +53,7 @@ public class CapabilityUpdateAndSync
 			}
 		}
 	}
-	
+
 	private static void sendTemperatureUpdate(PlayerEntity player)
 	{
 		if (player instanceof ServerPlayerEntity)
@@ -50,16 +65,16 @@ public class CapabilityUpdateAndSync
 			NetworkHandler.INSTANCE.send(PacketDistributor.PLAYER.with(() -> (ServerPlayerEntity)player), packet);
 		}
 	}
-	
+
 	@SubscribeEvent
-	public static void syncCapabilitiesOnLogin(PlayerLoggedInEvent event)
+	public static void syncCapabilitiesOnDimensionChange(PlayerChangedDimensionEvent event)
 	{
 		PlayerEntity player = event.getPlayer();
 		sendTemperatureUpdate(player);
 	}
-	
+
 	@SubscribeEvent
-	public static void syncCapabilitiesOnDimensionChange(PlayerChangedDimensionEvent event)
+	public static void syncCapabilitiesOnLogin(PlayerLoggedInEvent event)
 	{
 		PlayerEntity player = event.getPlayer();
 		sendTemperatureUpdate(player);
