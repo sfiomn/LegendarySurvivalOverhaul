e9cc4f589473d063d5f56bbd40bfd78523421baf
Icey <Penguixia@gmail.com>
2021-01-16 09:01:58 -0500

    reworked RenderUtils to require a matrix stack in its inputs. not sure if this is how i'm supposed to integrate it but it seems to work fine!

--- src/main/java/icey/survivaloverhaul/client/hud/temperature/TemperatureGUI.java
+++ src/main/java/icey/survivaloverhaul/client/hud/temperature/TemperatureGUI.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+import javax.annotation.Nullable;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 
@@ -19,6 +22,7 @@ import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.vector.Matrix4f;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
@@ -48,7 +52,9 @@ public class TemperatureGUI
 	private static int frameCounter = -1;
 	private static boolean risingTemperature = false;
 	private static boolean startAnimation = false;
-	private static boolean shakeSide = false;
+	
+	public static boolean shakeSide = false;
+	
 	
 	@SubscribeEvent
 	public static void renderHud(RenderGameOverlayEvent event)
@@ -60,40 +66,163 @@ public class TemperatureGUI
 			int scaledWidth = mc.getMainWindow().getScaledWidth();
 			int scaledHeight = mc.getMainWindow().getScaledHeight();
 			
+			rand.setSeed((long) (updateCounter * 445));
+			
 			bind(ICONS);
 			TemperatureCapability cap = TemperatureCapability.getTempCapability(mc.player);
 			
-			renderTemperatureGui(cap, scaledWidth, scaledHeight);
+			renderTemperatureGui(event.getMatrixStack(), cap, scaledWidth, scaledHeight);
 		}
 	}
 	
-	// @SubscribeEvent
-	public static void onClientTick(TickEvent.ClientTickEvent event)
-	{
-		
-	}
-	
-	public static void renderTemperatureGui(TemperatureCapability cap, int width, int height)
+	public static void renderTemperatureGui(MatrixStack matrix, TemperatureCapability cap, int width, int height)
 	{
 		RenderSystem.enableBlend();
 		
-		switch (Config.BakedConfigValues.temperatureDisplayMode)
+			drawAsSymbol(matrix, cap, width, height);
+		
+		
+		RenderSystem.disableBlend();
+	}
+	
+	public static void drawAsSymbol(MatrixStack matrix, TemperatureCapability cap, int width, int height)
+	{
+		int x = width / 2 - 8;
+		int y = height - 54;
+		
+		int xOffset = 0;
+		int yOffset = 0;
+		
+		int temperature = cap.getTemperatureLevel();
+		
+		TemperatureEnum tempEnum = cap.getTemperatureEnum(); 
+		
+		IconPair icon;
+		
+		byte shakeFrequency = 0;
+		
+		switch (tempEnum)
 		{
-		case SYMBOL:
-			int x = width / 2 - 8;
-			int y = height - 54;
+		case HEAT_STROKE:
+			icon = IconPair.FIRE;
 			
-			IconPair icon = IconPair.NORMAL;
+			if (temperature == TemperatureEnum.HEAT_STROKE.getLowerBound())
+			{
+				shakeFrequency = 0;
+			}
+			else if (temperature >= TemperatureEnum.HEAT_STROKE.getMiddle())
+			{
+				shakeFrequency = 1;
+			}
+			else
+			{
+				shakeFrequency = 2;
+			}
 			
-			RenderUtil.drawTexturedModelRect(x, y, textureWidth * icon.getIconIndex(), texturePosY, textureWidth, textureHeight);
-			RenderUtil.drawTexturedModelRect(x, y, textureWidth * icon.getIconHolder(), texturePosY, textureWidth, textureHeight);
+			break;
+		case HOT:
+			icon = IconPair.ABOVE_NORMAL;
+			break;
+		case NORMAL:
+			icon = IconPair.NORMAL;
+			break;
+		case COLD:
+			icon = IconPair.BELOW_NORMAL;
+			break;
+		case FROSTBITE:
+			icon = IconPair.SNOWFLAKE;
+			
+			if (temperature == TemperatureEnum.FROSTBITE.getUpperBound())
+			{
+				shakeFrequency = 0;
+			}
+			else if (temperature > TemperatureEnum.FROSTBITE.getMiddle() + 1)
+			{
+				shakeFrequency = 2;
+			}
+			else
+			{
+				shakeFrequency = 1;
+			}
 			
 			break;
 		default:
+			icon = IconPair.NORMAL;
 			break;
 		}
 		
-		RenderSystem.disableBlend();
+		boolean isDying = (TemperatureEnum.FROSTBITE.getMiddle() >= cap.getTemperatureLevel()  || TemperatureEnum.HEAT_STROKE.getMiddle() < cap.getTemperatureLevel());
+		
+		if (shakeFrequency > 0)
+		{
+			if (updateCounter % shakeFrequency == 0 && !isDying)
+			{
+				int shakeCheck = updateCounter / shakeFrequency;
+				
+				if (shakeCheck % 2 == 0)
+						shakeSide = true;
+				else
+						shakeSide = false;
+				
+				xOffset = shakeSide ? 1 : -1;
+			}
+			else if (isDying)
+			{
+				xOffset = rand.nextFloat() > 0.5f ? 1 : -1;
+				yOffset = rand.nextFloat() > 0.5f ? 1 : -1;
+			}
+		}
+		
+		xOffset += Config.BakedConfigValues.temperatureDisplayOffsetX;
+		yOffset += Config.BakedConfigValues.temperatureDisplayOffsetY;
+		
+		if (frameCounter >= 22)
+		{
+			icon = icon.getFlashVariant();
+		}
+		
+		Matrix4f m4f = matrix.getLast().getMatrix();
+		
+		RenderUtil.drawTexturedModelRect(m4f, x + xOffset, y + yOffset, textureWidth * icon.getIconIndex(), texturePosY, textureWidth, textureHeight);
+		RenderUtil.drawTexturedModelRect(m4f, x + xOffset, y + yOffset, textureWidth * icon.getIconHolder(), texturePosY, textureWidth, textureHeight);
+		
+		if (oldTemperature == -1)
+		{
+			oldTemperature = temperature;
+		}
+		
+		if (oldTemperature != temperature)
+		{
+			risingTemperature = oldTemperature < temperature;
+			oldTemperature = temperature;
+			startAnimation = true;
+		}
+		
+		int ovrXOffset = textureWidth * ((frameCounter / 2) - 1);
+		int ovrYOffset = texturePosY + (textureHeight * (risingTemperature ? 1 : 2));
+
+		RenderUtil.drawTexturedModelRect(m4f, x + xOffset, y + yOffset, ovrXOffset, ovrYOffset, textureWidth, textureHeight);
+	}
+	
+	@SubscribeEvent
+	public static void onClientTick(TickEvent.ClientTickEvent event)
+	{
+		if (event.phase == TickEvent.Phase.END)
+		{
+			if (!mc.isGamePaused())
+			{
+				updateCounter++;
+				
+				if (frameCounter >= 0)
+						frameCounter--;
+				
+				if (startAnimation)
+				{
+					frameCounter = 24;
+					startAnimation = false;
+				}
+			}
+		}
 	}
 	
 	private static void bind(ResourceLocation resource)
@@ -106,11 +235,11 @@ public class TemperatureGUI
 		NORMAL(0, 3),
 		FIRE(1, 4),
 		SNOWFLAKE(2, 5),
+		ABOVE_NORMAL(0, 11),
+		BELOW_NORMAL(0, 12),
 		NORMAL_FLASH(0, 6),
 		FIRE_FLASH(1, 7),
 		SNOWFLAKE_FLASH(2, 8),
-		ABOVE_NORMAL(0, 11),
-		BELOW_NORMAL(0, 12),
 		ABOVE_NORMAL_FLASH(0, 9),
 		BELOW_NORMAL_FLASH(0, 10);
 		
@@ -131,6 +260,25 @@ public class TemperatureGUI
 		public int getIconHolder()
 		{
 			return this.iconHolder;
+		}
+		
+		public IconPair getFlashVariant()
+		{
+			switch(this)
+			{
+			case NORMAL:
+				return NORMAL_FLASH;
+			case FIRE:
+				return FIRE_FLASH;
+			case SNOWFLAKE:
+				return SNOWFLAKE_FLASH;
+			case ABOVE_NORMAL:
+				return ABOVE_NORMAL_FLASH;
+			case BELOW_NORMAL:
+				return BELOW_NORMAL_FLASH;
+			default:
+				return this;
+			}
 		}
 	}
 }
--- src/main/java/icey/survivaloverhaul/util/RenderUtil.java
+++ src/main/java/icey/survivaloverhaul/util/RenderUtil.java
@@ -5,32 +5,28 @@ import org.lwjgl.opengl.GL11;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.util.math.vector.Matrix4f;
 
 public class RenderUtil
 {
-	public static void drawTexturedModelRect(float x, float y, int texX, int texY, int width, int height)
+	public static void drawTexturedModelRect(Matrix4f matrix, float x, float y, int texX, int texY, int width, int height)
 	{
 		float f = 0.00390625f;
 		float f1 = 0.00390625f;
-		double z = 0.0d;
+		float z = 0.0f;
 		
 		Tessellator tessellator = Tessellator.getInstance();
 		BufferBuilder bufferBuilder = tessellator.getBuffer();
 		
 		bufferBuilder.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
-		bufferBuilder.pos(x, y + height, z)
+		bufferBuilder.pos(matrix, x, y + height, z)
 				.tex((texX * f), (texY + height) * f1).endVertex();
-		bufferBuilder.pos((x + width), y + height, z)
+		bufferBuilder.pos(matrix, (x + width), y + height, z)
 				.tex((texX + width) * f, (texY + height) * f1).endVertex();
-		bufferBuilder.pos((x + width), y, z)
+		bufferBuilder.pos(matrix, (x + width), y, z)
 				.tex((texX + width) * f,(texY * f1)).endVertex();
-		bufferBuilder.pos(x, y, z)
+		bufferBuilder.pos(matrix, x, y, z)
 				.tex((texX * f), (texY * f1)).endVertex();
 		tessellator.draw();
-	}
-	
-	public static void drawTexturedModelSquare(float x, float y, int texX, int texY, int side)
-	{
-		drawTexturedModelRect(x, y, texX, texY, side, side);
 	}
 }
