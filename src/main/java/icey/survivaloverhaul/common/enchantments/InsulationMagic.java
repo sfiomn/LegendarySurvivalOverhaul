6c52d2df7b02af507d603aec5c938a8b697d908c
budgiegamer <sedricfoley3@gmail.com>
2021-01-31 01:57:13 -0500

    Testing new enchant modifiers

--- src/main/java/icey/survivaloverhaul/common/enchantments/InsulationMagic.java
+++ src/main/java/icey/survivaloverhaul/common/enchantments/InsulationMagic.java
@@ -8,11 +8,12 @@ import net.minecraft.enchantment.EnchantmentType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.potion.EffectInstance;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-
-@Mod.EventBusSubscriber(modid = Main.MOD_ID)
+//value = dist.client is fine
+//@Mod.EventBusSubscriber(value = Dist.CLIENT, modid = Main.MOD_ID)
 public class InsulationMagic extends GenericMagic
 {	
 	private static EquipmentSlotType[] slots = new EquipmentSlotType[] {EquipmentSlotType.HEAD, EquipmentSlotType.CHEST, EquipmentSlotType.LEGS, EquipmentSlotType.FEET};
@@ -27,36 +28,32 @@ public class InsulationMagic extends GenericMagic
 		this.MT = MT;
 	}
 	private static int skip = 0;
-	@SubscribeEvent
-	public static void Effect(LivingUpdateEvent event) 
-	{
-		LivingEntity entity = event.getEntityLiving();
-		if (entity.getEntityWorld().isRemote)
-			return;
-		/*
-		int ab = EnchantmentHelper.getMaxEnchantmentLevel(EnchantRegistry.ModEnchants.ADAPTIVE_BARRIER, entity),
-			h = EnchantmentHelper.getMaxEnchantmentLevel(EnchantRegistry.ModEnchants.THERMAL_BARRIER, entity),
-			c = EnchantmentHelper.getMaxEnchantmentLevel(EnchantRegistry.ModEnchants.COLD_BARRIER, entity);
-		if (skip == 0) 
-		{
-			//apply effects or modifiers
-			if (ab > 0) 
-			{
-				entity.addPotionEffect(new EffectInstance(EffectRegistry.ModEffects.COLD_RESISTANCE, 500, 0, true, false));
-				entity.addPotionEffect(new EffectInstance(EffectRegistry.ModEffects.HEAT_RESISTANCE, 500, 0, true, false));
-			}
-			else if (h > 0)
-				entity.addPotionEffect(new EffectInstance(EffectRegistry.ModEffects.HEAT_RESISTANCE, 500, 0, true, false));
-			else if (c > 0)
-				entity.addPotionEffect(new EffectInstance(EffectRegistry.ModEffects.COLD_RESISTANCE, 500, 0, true, false));
-			skip++;// re-apply effects after a few seconds
-		}
-		else if(skip == 100)
-			skip = 0;
-		else
-			skip++;
-		*/
-	}
+	//@SubscribeEvent
+//	public static void Effect(LivingUpdateEvent event) 
+//	{
+//		LivingEntity entity = event.getEntityLiving();
+//		if (skip == 0) 
+//		{
+//			int ab = EnchantmentHelper.getMaxEnchantmentLevel(EnchantRegistry.ModEnchants.ADAPTIVE_BARRIER, entity),
+//			h = EnchantmentHelper.getMaxEnchantmentLevel(EnchantRegistry.ModEnchants.THERMAL_BARRIER, entity),
+//			c = EnchantmentHelper.getMaxEnchantmentLevel(EnchantRegistry.ModEnchants.COLD_BARRIER, entity);
+//			//apply effects or modifiers
+//			if (ab > 0) 
+//			{
+//				entity.addPotionEffect(new EffectInstance(EffectRegistry.ModEffects.COLD_RESISTANCE, 500, 0, true, false));
+//				entity.addPotionEffect(new EffectInstance(EffectRegistry.ModEffects.HEAT_RESISTANCE, 500, 0, true, false));
+//			}
+//			else if (h > 0)
+//				entity.addPotionEffect(new EffectInstance(EffectRegistry.ModEffects.HEAT_RESISTANCE, 500, 0, true, false));
+//			else if (c > 0)
+//				entity.addPotionEffect(new EffectInstance(EffectRegistry.ModEffects.COLD_RESISTANCE, 500, 0, true, false));
+//			skip++;// re-apply effects after a few seconds
+//		}
+//		else if(skip == 100)
+//			skip = 0;
+//		else
+//			skip++;
+//	}
 	//not used atm
 	public enum MagicType
 	{
--- src/main/java/icey/survivaloverhaul/common/temperature/ArmorModifier.java
+++ src/main/java/icey/survivaloverhaul/common/temperature/ArmorModifier.java
@@ -5,6 +5,7 @@ import java.util.List;
 import icey.survivaloverhaul.Main;
 import icey.survivaloverhaul.api.config.json.temperature.JsonArmorIdentity;
 import icey.survivaloverhaul.api.temperature.ModifierBase;
+import icey.survivaloverhaul.api.temperature.TemperatureEnum;
 import icey.survivaloverhaul.api.temperature.TemperatureUtil;
 import icey.survivaloverhaul.config.Config;
 import icey.survivaloverhaul.config.json.JsonConfig;
@@ -26,31 +27,43 @@ public class ArmorModifier extends ModifierBase
 	public float getPlayerInfluence(PlayerEntity player)
 	{
 		float value = 0.0f;
-		value += checkArmorSlot(player.getItemStackFromSlot(EquipmentSlotType.HEAD));
-		value += checkArmorSlot(player.getItemStackFromSlot(EquipmentSlotType.CHEST));
-		value += checkArmorSlot(player.getItemStackFromSlot(EquipmentSlotType.LEGS));
-		value += checkArmorSlot(player.getItemStackFromSlot(EquipmentSlotType.FEET));
+		int temp = TemperatureUtil.getWorldTemperature(player.world, player.getPosition());
+		//float temp = player.world.getBiome(player.getPosition()).getTemperature();
+		value += checkArmorSlot(player.getItemStackFromSlot(EquipmentSlotType.HEAD), temp);
+		value += checkArmorSlot(player.getItemStackFromSlot(EquipmentSlotType.CHEST), temp);
+		value += checkArmorSlot(player.getItemStackFromSlot(EquipmentSlotType.LEGS), temp);
+		value += checkArmorSlot(player.getItemStackFromSlot(EquipmentSlotType.FEET), temp);
+		System.out.println("Value:"+ value +" Temp: "+ temp);
 		return value;
 	}
 	
-	private float checkArmorSlot(ItemStack stack)
+	private float checkArmorSlot(ItemStack stack, int temp)
 	{
 		if (stack.isEmpty())
 				return 0.0f;
 		
 		float sum = 0.0f;
-		
+		int adaptiveLevel = EnchantmentHelper.getEnchantmentLevel(EnchantRegistry.ModEnchants.ADAPTIVE_BARRIER, stack);
 		int coolingLevel = EnchantmentHelper.getEnchantmentLevel(EnchantRegistry.ModEnchants.COLD_BARRIER, stack);
 		int heatingLevel = EnchantmentHelper.getEnchantmentLevel(EnchantRegistry.ModEnchants.THERMAL_BARRIER, stack);
-		
-		if (coolingLevel > 0)
+		if (adaptiveLevel > 0) 
+		{
+			//dont know the max temperature
+			//lower limit <-> upper limit
+			if (temp >= TemperatureEnum.FROSTBITE.getUpperBound())
+				sum -= adaptiveLevel * Config.Baked.enchantmentMultiplier;
+			else if (temp >= 0.20)
+				sum += adaptiveLevel * Config.Baked.enchantmentMultiplier;
+		}
+		else
+			if (coolingLevel > 0)
 			sum -= coolingLevel * Config.Baked.enchantmentMultiplier;
 		else if (heatingLevel > 0)
 			sum += heatingLevel * Config.Baked.enchantmentMultiplier;
-		
+		System.out.println("Before "+ sum);
 		sum += processStackJson(stack);
 		sum += TemperatureUtil.getArmorTemperatureTag(stack);
-		
+		System.out.println("After "+ sum);
 		return sum;
 	}
 	
