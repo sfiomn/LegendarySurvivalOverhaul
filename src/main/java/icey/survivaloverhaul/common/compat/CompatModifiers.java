7360271df170a2177515395c3a6970dae790c742
Icey <Penguixia@gmail.com>
2021-02-01 14:06:45 -0500

    began work on curios compatibility, added artifacts mod for testing purposes

--- src/main/java/icey/survivaloverhaul/common/compat/CompatModifiers.java
+++ src/main/java/icey/survivaloverhaul/common/compat/CompatModifiers.java
@@ -2,6 +2,7 @@ package icey.survivaloverhaul.common.compat;
 
 import icey.survivaloverhaul.Main;
 import icey.survivaloverhaul.api.temperature.ModifierBase;
+import icey.survivaloverhaul.common.compat.curios.CuriosModifier;
 import icey.survivaloverhaul.common.compat.sereneseasons.SereneSeasonModifier;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -12,11 +13,14 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 public class CompatModifiers
 {
 	public static final ModifierBase SERENE_SEASONS = new SereneSeasonModifier();
+	public static final ModifierBase CURIOS = new CuriosModifier();
 	
 	@SubscribeEvent
 	public static void registerModifiers(RegistryEvent.Register<ModifierBase> event)
 	{
 		if (Main.sereneSeasonsLoaded)
-				event.getRegistry().register(SERENE_SEASONS);
+			event.getRegistry().register(SERENE_SEASONS);
+		if (Main.curiosLoaded)
+			event.getRegistry().register(CURIOS);
 	}
 }
--- src/main/java/icey/survivaloverhaul/common/compat/curios/CuriosModifier.java
+++ src/main/java/icey/survivaloverhaul/common/compat/curios/CuriosModifier.java
@@ -1,6 +1,94 @@
 package icey.survivaloverhaul.common.compat.curios;
 
-public class CuriosModifier
-{
+import java.util.List;
 
+import icey.survivaloverhaul.Main;
+import icey.survivaloverhaul.api.config.json.temperature.JsonArmorIdentity;
+import icey.survivaloverhaul.api.temperature.ModifierBase;
+import icey.survivaloverhaul.api.temperature.TemperatureUtil;
+import icey.survivaloverhaul.config.json.JsonConfig;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.IItemHandlerModifiable;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.type.util.ICuriosHelper;
+
+public class CuriosModifier extends ModifierBase
+{
+	public CuriosModifier()
+	{
+		super();
+		this.setRegistryName(Main.MOD_ID, "compat/curios");
+	}
+	
+	@Override
+	public float getPlayerInfluence(PlayerEntity player)
+	{
+		if (!Main.curiosLoaded)
+			return 0.0f;
+		
+		try
+		{
+			return getUncaughtPlayerInfluence(player);
+		}
+		catch (Exception e)
+		{
+			Main.LOGGER.error("An error has occured with Curios compatability, disabling modifier", e);
+			Main.curiosLoaded = false;
+			
+			return 0.0f;
+		}
+	}
+	
+	public float getUncaughtPlayerInfluence(PlayerEntity player)
+	{
+		ICuriosHelper helper = CuriosApi.getCuriosHelper();
+		
+		LazyOptional<IItemHandlerModifiable> lazyOptional = helper.getEquippedCurios(player);
+		
+		if (lazyOptional.isPresent())
+		{
+			IItemHandler itemHandler = lazyOptional.resolve().get();
+			
+			float sum = 0.0f;
+			
+			for (int i = 0; i < itemHandler.getSlots(); i++)
+			{
+				final ItemStack stack = itemHandler.getStackInSlot(i);
+				
+				if (!stack.isEmpty())
+				{
+					sum += processStackJson(stack);
+					sum += TemperatureUtil.getArmorTemperatureTag(stack);
+				}
+			}
+			
+			return sum;
+		}
+		
+		return 0.0f;
+	}
+	
+	private float processStackJson(ItemStack stack)
+	{
+		List<JsonArmorIdentity> identity = JsonConfig.armorTemperatures.get(stack.getItem().getRegistryName().toString());
+		
+		if (identity != null)
+		{
+			for (JsonArmorIdentity jai : identity)
+			{
+				if (jai == null)
+						continue;
+				
+				if (jai.matches(stack))
+				{
+					return jai.temperature;
+				}
+			}
+		}
+		
+		return 0.0f;
+	}
 }
