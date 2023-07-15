63cbcd286ff02dd90f27481773baeda8977ce4b0
Simon Fontaine <simon.fontaine91@gmail.com>
2023-07-15 20:39:18 +0200

    Replace the biome type config by the biome category for ferns spawn

--- src/main/java/sfiomn/legendarysurvivaloverhaul/common/world/gen/ModFlowerGeneration.java
+++ src/main/java/sfiomn/legendarysurvivaloverhaul/common/world/gen/ModFlowerGeneration.java
@@ -1,58 +1,50 @@
 package sfiomn.legendarysurvivaloverhaul.common.world.gen;
 
-import net.minecraft.util.RegistryKey;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
+import sfiomn.legendarysurvivaloverhaul.LegendarySurvivalOverhaul;
 import sfiomn.legendarysurvivaloverhaul.config.Config;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.Set;
 import java.util.function.Supplier;
 
 public class ModFlowerGeneration {
 
     public static void generateFlowers(final BiomeLoadingEvent event) {
 
-        RegistryKey<Biome> key = RegistryKey.create(Registry.BIOME_REGISTRY, Objects.requireNonNull(event.getName()));
-        Set<BiomeDictionary.Type> types = BiomeDictionary.getTypes(key);
+        ResourceLocation biomeName = event.getName();
+        Biome.Category biomeCagegory = event.getCategory();
 
         boolean canIceFernSpawn = false;
         boolean canSunFernSpawn = false;
 
-        if (Config.Baked.iceFernBiomeNames.contains(key.location().toString())) {
-            canIceFernSpawn = true;
-            // LegendarySurvivalOverhaul.LOGGER.debug("Add ice fern in biome " + key.location());
-        } else {
-            for (BiomeDictionary.Type type : types) {
-                if (Config.Baked.iceFernBiomeTypes.contains(type.getName())) {
-                    canIceFernSpawn = true;
-                    // LegendarySurvivalOverhaul.LOGGER.debug("Add ice fern biome " + key.location() + " with type " + type.getName());
-                }
-            }
-        }
-        if (Config.Baked.sunFernBiomeNames.contains(key.location().toString())) {
-            canSunFernSpawn = true;
-            // LegendarySurvivalOverhaul.LOGGER.debug("Add sun fern in biome " + key.location());
-        } else {
-            for (BiomeDictionary.Type type : types) {
-                if (Config.Baked.sunFernBiomeTypes.contains(type.getName())) {
-                    canSunFernSpawn = true;
-                    // LegendarySurvivalOverhaul.LOGGER.debug("Add sun fern biome " + key.location() + " with type " + type.getName());
-                }
-            }
+        if (biomeName != null) {
+            canIceFernSpawn = Config.Baked.iceFernBiomeNames.contains(biomeName.toString());
+            canSunFernSpawn = Config.Baked.sunFernBiomeNames.contains(biomeName.toString());
         }
 
+        //  biomeCategory.getName() returns the lowercase name of the category
+        //  biomeCategory.toString() is the normal uppercase name of the category
+        if (!canIceFernSpawn)
+            canIceFernSpawn = Config.Baked.iceFernBiomeCategories.contains(biomeCagegory.getName()) ||
+                    Config.Baked.iceFernBiomeCategories.contains(biomeCagegory.toString());
+
+        if (!canSunFernSpawn)
+            canSunFernSpawn = Config.Baked.sunFernBiomeCategories.contains(biomeCagegory.getName()) ||
+                    Config.Baked.sunFernBiomeCategories.contains(biomeCagegory.toString());
+
         if (canIceFernSpawn) {
+            LegendarySurvivalOverhaul.LOGGER.debug("Generate ice fern in biome " + biomeName);
             List<Supplier<ConfiguredFeature<?, ?>>> base = event.getGeneration().getFeatures(GenerationStage.Decoration.VEGETAL_DECORATION);
 
             base.add(() -> ModConfiguredFeatures.ICE_FERN_CONFIG);
         }
+
         if (canSunFernSpawn) {
+            LegendarySurvivalOverhaul.LOGGER.debug("Generate sun fern in biome " + biomeName);
             List<Supplier<ConfiguredFeature<?, ?>>> base = event.getGeneration().getFeatures(GenerationStage.Decoration.VEGETAL_DECORATION);
 
             base.add(() -> ModConfiguredFeatures.SUN_FERN_CONFIG);
