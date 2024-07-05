d6f5eac654ff8fa1435a0467e1f2e9765dfe432d
Sfiomn <simon.fontaine91@gmail.com>
2024-07-05 12:29:50 +0200

    Fix typo, removing panting/shivering sound in creative mode

--- src/main/java/sfiomn/legendarysurvivaloverhaul/client/sounds/TemperatureEffectSound.java
+++ src/main/java/sfiomn/legendarysurvivaloverhaul/client/sounds/TemperatureEffectSound.java
@@ -19,7 +19,7 @@ public class TemperatureEffectSound {
     private static boolean reset;
 
     public static void tickPlay(PlayerEntity player) {
-        if (player == null || !player.isAlive() || player.isSpectator() || player.isSpectator()) {
+        if (player == null || !player.isAlive() || player.isSpectator() || player.isCreative()) {
             return;
         }
 
